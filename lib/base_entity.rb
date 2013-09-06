e7e1b2f7bf3c0efacd0847693142abf178efb69d
Nando Sola <nando@robotchrist.com>
2013-09-06 19:29:11 +0200

    Refactored BaseEntity.children_type [sequel-datasets]

--- lib/base_entity.rb
+++ lib/base_entity.rb
@@ -127,9 +127,8 @@ class BaseEntity < IdPk
 
   def self.children_type(child_attr)
     # TODO change ChildReference.type for the real thing
-    # TODO refactor (same method @253)
     if self.child_references.include?(child_attr)
-      module_path = self.class.to_s.split('::')
+      module_path = self.to_s.split('::')
       child_literal = child_attr.to_s.singularize.camelize
 
       child_class = if 1 == module_path.size
@@ -251,18 +250,7 @@ class BaseEntity < IdPk
       # Single-entity methods:
 
       define_method("make_#{child.to_s.singularize}".to_sym) do |in_h|
-        module_path = self.class.to_s.split('::')
-        child_literal = child.to_s.singularize.camelize
-
-        child_class = if 1 == module_path.size
-                        Object.const_get(child_literal)
-                      elsif 1 < module_path.size
-                        child_path = module_path[0..-2] << child_literal
-                        child_path.reduce(Object){ |m,c| m.const_get(c) }
-                      else
-                        raise RuntimeError, "Cannot determine #{child_literal} namespace for parent path #{module_path.join('::')}"
-                      end
-
+        child_class = self.class.children_type(child)
         a_child = child_class.new(in_h, self)
         send("#{child}<<".to_sym, a_child)
 
