1474120cdc5b0fdbe1372cce49079b1eb89b6189
Nando Sola <nando@robotchrist.com>
2014-03-26 10:41:57 +0100

    Moves SchemaUtils to mappers directory

--- lib/persistence/persistence.rb
+++ lib/persistence/persistence.rb
@@ -3,7 +3,7 @@
 require_relative 'exceptions'
 require_relative '../model/shared_version'
 require_relative 'persistence_service'
-require_relative 'schema_utils'
+require_relative 'mappers/schema_utils'
 require_relative 'mappers/default'
 require_relative 'mappers/entity'
 require_relative 'mappers/value'
