6876f91b9ea4fcae458d2f4f9058f51adc74654b
Mario Camou <mcamou@abstra.cc>
2014-04-02 15:21:28 +0200

    More test cleanup

--- spec/020_simple_entity_spec.rb
+++ spec/020_simple_entity_spec.rb
@@ -17,7 +17,9 @@ describe 'BaseEntity' do
       end
 
       expect(User.attributes).to eq(before_attrs)
+    end
 
+    it 'has the proper methods' do
       a_user = User.new
       expect(a_user).to respond_to(:id)
       expect(a_user).to respond_to(:id=)
@@ -30,10 +32,35 @@ describe 'BaseEntity' do
       expect(a_user).to_not respond_to(:my_thing=)
     end
 
-    it 'does not allow to be initialized with bogus attributes or values' do
-      expect {User.new({funny:false})}.to raise_error(ArgumentError)
-      expect {User.new({name:1337})}.to raise_error(ArgumentError)
-      expect {User.new({reference:'not a reference'})}.to raise_error(ArgumentError)
+    it 'validates its attributes' do
+      new_user = User.new
+      my_reference = Reference.new({name:'test'})
+
+      expect(my_reference.name).to eq('test')
+      expect(new_user.reference).to be_nil
+      expect {new_user.reference = 'foo'}.to raise_error(ArgumentError)
+      expect {new_user.reference = my_reference}.to_not raise_error
+      expect {new_user.email = 42}.to raise_error(ArgumentError)
+      expect {new_user.name = 1337}.to raise_error(ArgumentError)
+    end
+
+    it 'can be fully serialized' do
+      a_user = User.fetch_by_id(1)
+      test_hash ={
+        :id => 1,
+        :name => 'Alice',
+        :email => 'alice@example.net',
+        :tstamp=> DateTime.strptime('2013-09-23T18:42:14+02:00', '%Y-%m-%dT%H:%M:%S%z'),
+        :password=>'$2a$10$hqlENYeHZYy9eYHnZ2ONH.5N9qnXV9uzXA/h27XCMq5HBytCLo6bm',
+        :active=>true,
+        :reference => nil,
+        :refers_to => nil,
+        :title => 'Esq.',
+        :_version => {id:1, _version:0, _version_created_at:DateTime.parse('2013-09-23T18:42:14+02:00'),
+                      _locked_by: nil, _locked_at: nil}
+      }
+
+      EntitySerializer.to_nested_hash(a_user).each { |k, v| expect(test_hash[k]).to eq(v) }
     end
   end
 
@@ -99,65 +126,45 @@ describe 'BaseEntity' do
   end
 
   describe '.new' do
-    it 'accepts empty or full-hash constructors and validates its attributes' do
+    describe 'with full-hash constructor' do
+      let(:norbert) { {:name => 'Norbert', :email => 'norb@example.net'} }
+      subject(:new_user) { User.new(norbert) }
 
-      norbert = {:name => 'Norbert', :email => 'norb@example.net'}
-      dilbert = {:name => 'Dilbert', :email => 'dilbert@example.net'}
+      it 'validates the attribute types' do
+        expect {User.new({:name => 'Catbert', :email => 1337})}.to raise_error(ArgumentError)
+        expect {User.new({:name => nil, :email => 'catbert@example.net'})}.to raise_error(ArgumentError)
+        expect {User.new({funny:false})}.to raise_error(ArgumentError)
+        expect {User.new({name:1337})}.to raise_error(ArgumentError)
+        expect {User.new({reference:'not a reference'})}.to raise_error(ArgumentError)
+      end
 
-      new_user = User.new(norbert)
-      another_user= User.new
+      it 'assigns values correctly' do
+        expect(new_user.id).to eq(nil)
+        expect(new_user.name).to eq('Norbert')
+        expect(new_user.email).to eq('norb@example.net')
+      end
 
-      expect(new_user).to respond_to(:id)
-      expect(new_user).to respond_to(:id=)
-      expect(new_user).to respond_to(:name)
-      expect(new_user).to respond_to(:name=)
-      expect(new_user).to respond_to(:email)
-      expect(new_user).to respond_to(:email=)
+      it 'is persisted correctly' do
+        new_user.reference = Reference.new(name: 'test')
 
-      my_reference = Reference.new({name:'test'})
-      expect(new_user).to respond_to(:reference)
-      expect(new_user).to respond_to(:reference=)
-      expect(my_reference.name).to eq('test')
-      expect(new_user.reference).to be_nil
-      expect {new_user.reference = 'foo'}.to raise_error(ArgumentError)
-      new_user.reference = my_reference
+        transaction = UnitOfWork::Transaction.new(EntityMapper::Sequel)
+        transaction.register_new(new_user)
+        transaction.commit
+        transaction.complete
 
-      expect {another_user.email = 42}.to raise_error(ArgumentError)
-      expect {another_user.name = 1337}.to raise_error(ArgumentError)
-      expect {User.new({:name => 'Catbert', :email => 1337})}.to raise_error(ArgumentError)
-      expect {User.new({:name => nil, :email => 'catbert@example.net'})}.to raise_error(ArgumentError)
-
-      another_user.make(dilbert)
-
-      expect(another_user.id).to eq(nil)
-      expect(another_user.name).to eq('Dilbert')
-      expect(another_user.email).to eq('dilbert@example.net')
-
-      expect(new_user.id).to eq(nil)
-      expect(new_user.name).to eq('Norbert')
-      expect(new_user.email).to eq('norb@example.net')
-
-      expect(another_user.id).to eq(nil)
-      expect(another_user.name).to eq('Dilbert')
-      expect(another_user.email).to eq('dilbert@example.net')
-
-      transaction = UnitOfWork::Transaction.new(EntityMapper::Sequel)
-
-      transaction.register_new(new_user)
-      transaction.register_new(another_user)
-      transaction.commit
-      transaction.complete
-      norb = User.fetch_by_name('Norbert')
-      expect(norb.first.email).to eq('norb@example.net')
-      expect(norb.first.reference).to_not be_nil
-      expect(User.fetch_by_name('Dilbert').first.email).to eq('dilbert@example.net')
+        norb = User.fetch_by_name('Norbert').first
+        expect(norb.email).to eq('norb@example.net')
+        expect(norb.reference).to be_a(Association::LazyEntityReference)
+        expect(norb.reference.resolved_entity.name).to eq('test')
+      end
     end
   end
 
   describe '.full_update' do
     it 'can be fully updated' do
-      user = User.fetch_by_name('Dilbert').first
+      user = User.fetch_by_name('Norbert').first
       id = user.id
+
       user.full_update({id:id, :name => 'Dogbert', :email => 'dogbert@example.net'})
       expect(user.name).to eq('Dogbert')
       expect(user.email).to eq('dogbert@example.net')
@@ -175,50 +182,37 @@ describe 'BaseEntity' do
     end
   end
 
-  pending 'Implement BasicEntityBuilder'
-  it 'can be fully serialized' do
-    a_user = User.fetch_by_id(1)
-    test_hash ={
-      :id => 1,
-      :name => 'Alice',
-      :email => 'alice@example.net',
-      :tstamp=> DateTime.strptime('2013-09-23T18:42:14+02:00', '%Y-%m-%dT%H:%M:%S%z'),
-      :password=>'$2a$10$hqlENYeHZYy9eYHnZ2ONH.5N9qnXV9uzXA/h27XCMq5HBytCLo6bm',
-      :active=>true,
-      :reference => nil,
-      :refers_to => nil,
-      :title => 'Esq.',
-      :_version => {id:1, _version:0, _version_created_at:DateTime.parse('2013-09-23T18:42:14+02:00'),
-                    _locked_by: nil, _locked_at: nil}
-    }
-
-    EntitySerializer.to_nested_hash(a_user).each { |k, v| expect(test_hash[k]).to eq(v) }
-  end
-
   describe '.immutable' do
-    it 'can return an immutable copy of itself' do
+    let(:a_user) { User.fetch_by_email('zaphod@example.net').first }
+    subject(:an_immutable_user) { a_user.immutable }
 
-      a_user = User.fetch_by_email('zaphod@example.net').first
-      an_immutable_user = a_user.immutable
+    describe 'returns an immutable copy of itself' do
+      it {should be_a(User::Immutable)}      
+    end
+    
+    describe 'has the same accessors as the original' do
+      it { should respond_to(:id)}
+      it { should respond_to(:name)}
+      it { should respond_to(:email)}
+    end
 
-      expect(an_immutable_user).to be_a(User::Immutable)
-
-      expect(an_immutable_user).to respond_to(:id)
-      expect(an_immutable_user).to_not respond_to(:id=)
-      expect(an_immutable_user).to respond_to(:name)
-      expect(an_immutable_user).to_not respond_to(:name=)
-      expect(an_immutable_user).to respond_to(:email)
-      expect(an_immutable_user).to_not respond_to(:email=)
-      expect(an_immutable_user).to_not respond_to(:reference)
-      expect(an_immutable_user).to_not respond_to(:reference=)
-      expect(an_immutable_user).to_not respond_to(:refers_to)
-      expect(an_immutable_user).to_not respond_to(:refers_to=)
-
-      expect(an_immutable_user).to_not respond_to(:my_thing)
-
+    it 'copies over the attribute values from the original' do
       expect(an_immutable_user.id).to eq(a_user.id)
       expect(an_immutable_user.name).to eq(a_user.name)
       expect(an_immutable_user.email).to eq(a_user.email)
+    end
+
+    describe 'doesn\'t have any mutators' do
+      it { should_not respond_to(:id=)}
+      it { should_not respond_to(:name=)}
+      it { should_not respond_to(:email=)}
+    end
+
+    describe 'doesn\'t have any references' do
+      it { should_not respond_to(:reference)}
+      it { should_not respond_to(:reference=)}
+      it { should_not respond_to(:refers_to)}
+      it { should_not respond_to(:refers_to=)}
     end
   end
 
